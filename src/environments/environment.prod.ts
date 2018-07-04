e37c0246be331f094e3e598b32fddaae57a18d36
alexsocheathson <alexson28@gmail.com>
2018-07-04 11:23:50 -0500

    Angular build for production. Deployed to firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAZpVO_9hHsbbF76ujTouyGKqkn2suNsM8',
+    authDomain: 'voting-app-14a91.firebaseapp.com',
+    databaseURL: 'https://voting-app-14a91.firebaseio.com',
+    projectId: 'voting-app-14a91',
+    storageBucket: 'voting-app-14a91.appspot.com',
+    messagingSenderId: '35033235008',
+  }
 };
