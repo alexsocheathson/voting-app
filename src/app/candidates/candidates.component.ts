a17f9bbcc0be6fc57480af760c35664e9d64593b
Alex Son <ason@bswing.com>
2018-07-05 16:09:06 -0500

    push

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 
 import { Candidate } from '../candidate';
-
 
 @Component({
   selector: 'app-candidates',
@@ -11,11 +10,34 @@ import { Candidate } from '../candidate';
   styleUrls: ['./candidates.component.css']
 })
 export class CandidatesComponent implements OnInit {
-  candidates: Observable<any[]>;
+  private candidateCollection: AngularFirestoreCollection<Candidate>;
+  candidates: Observable<Candidate[]>;
+
+  selectedCandidate: Candidate;
 
   constructor(db: AngularFirestore) {
-    this.candidates = db.collection('candidates').valueChanges();
+    this.candidateCollection = db.collection<Candidate>('candidates');
+    this.candidates = this.candidateCollection.valueChanges();
+    // this.addCandidate(5, 'Bot', 'Pushed to database', 'N/A');
   }
+
+  addCandidate(id: number, name: string, information: string, position: string) {
+    const candidate: Candidate = {id, name, information, position};
+    this.candidateCollection.add(candidate);
+  }
+
+  deleteCandifate(candidate: Candidate){
+    this.candidate.
+  }
+
+  // testAddCandidate() {
+  //   const candidate: Candidate = {id: 100, name: 'Buttons', information: 'button click', position: 'QA'};
+  //   this.candidateCollection.add(candidate);
+  // }
+
+  // onSelect(candidate: Candidate): void {
+  //   this.selectedCandidate = candidate;
+  // }
 
   ngOnInit() {
   }
