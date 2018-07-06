e3c30d8bcc025820d121ce6398c5b23cd7315125
Alex Son <ason@bswing.com>
2018-07-06 13:49:55 -0500

    Firebase deleting succesfull

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentChangeAction} from 'angularfire2/firestore';
+import { Observable } from 'rxjs/';
+import 'rxjs/add/operator/map';
 
 import { Candidate } from '../candidate';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-candidates',
@@ -17,8 +19,18 @@ export class CandidatesComponent implements OnInit {
 
   constructor(db: AngularFirestore) {
     this.candidateCollection = db.collection<Candidate>('candidates');
-    this.candidates = this.candidateCollection.valueChanges();
+    this.candidates = this.candidateCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Candidate;
+        const docID = a.payload.doc.id;
+        return {docID, ...data};
+      }))
+    );
     // this.addCandidate(5, 'Bot', 'Pushed to database', 'N/A');
+  }
+  onSelect(candidate: Candidate): void {
+    console.log(candidate);
+    this.selectedCandidate = candidate;
   }
 
   addCandidate(id: number, name: string, information: string, position: string) {
@@ -26,19 +38,19 @@ export class CandidatesComponent implements OnInit {
     this.candidateCollection.add(candidate);
   }
 
-  deleteCandifate(candidate: Candidate){
-    this.candidate.
+  updateCandidate(selectedCandidate) {
+    this.candidateCollection.doc(selectedCandidate.docID).ref.update({"name": "Yo"});
   }
+
+  deleteCandidate(selectedCandidate) {
+    // auto generated belongs in the path name
+    this.candidateCollection.doc(selectedCandidate.docID).ref.delete();
+}
 
   // testAddCandidate() {
   //   const candidate: Candidate = {id: 100, name: 'Buttons', information: 'button click', position: 'QA'};
   //   this.candidateCollection.add(candidate);
   // }
-
-  // onSelect(candidate: Candidate): void {
-  //   this.selectedCandidate = candidate;
-  // }
-
   ngOnInit() {
   }
 
