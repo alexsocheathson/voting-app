7dc3b322ab9e10574a3106750c483709b5b7ff39
alexsocheathson <alexson28@gmail.com>
2018-07-13 18:11:17 -0500

    removed firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,6 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyAZpVO_9hHsbbF76ujTouyGKqkn2suNsM8',
-    authDomain: 'voting-app-14a91.firebaseapp.com',
-    databaseURL: 'https://voting-app-14a91.firebaseio.com',
-    projectId: 'voting-app-14a91',
-    storageBucket: 'voting-app-14a91.appspot.com',
-    messagingSenderId: '35033235008',
-  }
-};
-
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
